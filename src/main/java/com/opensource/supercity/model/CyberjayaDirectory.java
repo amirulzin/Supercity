e2a447c6e3831f71e790629cab24d8c48c2ffb20
Amirul Zin <amirul.mdzin@gmail.com>
2015-08-21 23:43:57 +0800

    Added ignore for raws, added more libs, and base class for first model

---
+++ src/main/java/com/opensource/supercity/model/CyberjayaDirectory.java
@@ -0,0 +1,13 @@
+package com.opensource.supercity.model;
+
+public class CyberjayaDirectory {
+    String id;
+    String category;
+    String premise;
+    String phone;
+    String email;
+    String website;
+    String address;
+    String latitude;
+    String longitude;
+}
